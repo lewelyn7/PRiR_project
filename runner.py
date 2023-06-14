4353bcac9b2f31b00e850761c8e249dc0bf907a8
lewelyn7 <karol.hamielec@gmail.com>
2023-06-14 14:46:04 +0200

    mpi working

--- runner.py
+++ runner.py
@@ -1,6 +1,16 @@
 import os
+import time
 import signal
 import sys
+import enum
+
+class Modes(enum.Enum):
+    OMP = 0
+    MPI = 1
+    HYBRID = 2
+    
+mode = Modes.OMP
+
 stop = False
 def signal_handler(sig, frame):
     global stop
@@ -9,7 +19,9 @@ def signal_handler(sig, frame):
 
 signal.signal(signal.SIGINT, signal_handler)
 
-sizes = [2**i for i in range(10,29, 2)]
+sizes = [2**i for i in range(12,29, 2)]
+sizes.append(2**29)
+time.sleep(3)
 
 for size in sizes:
     for attempt in range(5):
@@ -18,5 +30,9 @@ for size in sizes:
                 print("exiting")
                 exit()
             print(f"executing with size: {size} attempt: {attempt+1}")
-            os.system(f"mpirun -n {thread}  --oversubscribe main_new {size} {thread} 1234")
+            if mode == Modes.MPI: 
+                os.system(f"mpirun -n {thread}  --oversubscribe main_mpi {size} {thread} 4096")
+            elif mode == Modes.OMP:
+                os.system(f"./main {size} {thread} 345")
+                
     
